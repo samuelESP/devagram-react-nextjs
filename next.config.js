ba2dd1d7998fad300a65cdc2e011a80b2de96b46
samuelESP <samuelnegocios1509@gmail.com>
2024-06-29 17:31:03 -0300

    estrutura base do projeto
    Variáveis, Imagens, fontes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
